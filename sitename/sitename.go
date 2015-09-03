4f7d5357098197d95267eb40db32d57a839f6a43
Joshua Sjoding <joshs@scjalliance.com>
2015-09-03 14:13:45 -0700

    sitename program now takes a list of computer names as arguments

--- sitename/sitename.go
+++ sitename/sitename.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"go.scj.io/adds"
 )
 
 func main() {
-	siteName, err := adds.GetSiteName("")
-	if err != nil {
-		panic(err)
+	if len(os.Args) < 2 {
+		lookup("")
+	} else {
+		for _, arg := range os.Args[1:] {
+			siteName, err := adds.GetSiteName(arg)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(siteName)
+		}
 	}
-	fmt.Println(siteName)
+}
+
+func lookup(computerName string) {
+	siteName, err := adds.GetSiteName(computerName)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(siteName)
+	}
 }
